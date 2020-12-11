8e64db37ef2c33ffe7aef7b4169e8c87bdf9dd7c
Марина <mika-klubnika@yandex.ru>
2020-12-11 10:30:49 +0300

    Доделано модальное окно и ховеры в блоке content

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,16 +1,23 @@
 var button = document.querySelector(".product-hit__link");
 var modal = document.querySelector(".modal");
+var emptyField = document.querySelector(".modal__base");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.add("modal__show");
 });
 
+emptyField.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.remove('modal__show');
+
+});
+
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (modal.classList.contains("modal__show")) {
-          modal.classList.toggle("modal__show");
-      }
+    evt.preventDefault();
+    if (modal.classList.contains("modal__show")) {
+      modal.classList.toggle("modal__show");
+    }
   }
 });
